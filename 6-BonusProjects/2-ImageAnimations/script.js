4f5744ec5768a075af4435725da4caf3b8a20edb
Zaron Gibson <zaron@zaron.ca>
2020-03-30 16:51:06 -0600

    Completed entire course. Have fun!

---
+++ 6-BonusProjects/2-ImageAnimations/script.js
@@ -0,0 +1,11 @@
+$('.img-container img')
+  .stop()
+  .animate(
+    {
+      width: 300,
+      height: 300,
+      top: 0,
+      left: 0
+    },
+    10000
+  );
---
+++ 7-BonusProjectsII/3-SubmitButtonAnimation/script.js
@@ -0,0 +1,6 @@
+var $submit = $('#btn-submit');
+
+$submit.click(function() {
+  $submit.text('');
+  $submit.addClass('submitted');
+});
